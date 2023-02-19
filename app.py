815e64a8f706dcf6899d16bf8ce5000bd8cd3add
gji2163 <31496913+gji2163@users.noreply.github.com>
2023-02-19 21:28:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,37 +1,52 @@
-from flask import Flask, request, jsonify
+"""
+A Ticketing System using Python Flask and REST API.
+"""
+import os
+from flask import Flask, request, render_template
+
 app = Flask(__name__)
+users = ["U1", "U2", "U3", "U4", "U5"]
+tickets = []
 
-# initialize users list and ticket counter
-users = ['U1', 'U2', 'U3']
-ticket_counter = 0
-
-# handle POST request to /ticket endpoint
 @app.route('/ticket', methods=['POST'])
 def create_ticket():
-    global ticket_counter, users
+    """Return a friendly HTTP greeting."""
+    message = [
+        'Both user_id and issue are required fields',
+        "It's running!",
+        'No users available to assign ticket'
+    ]
 
-    # parse request body for user_id and issue
-    user_id = request.json.get('user_id')
-    issue = request.json.get('issue')
-
-    # validate request body
-    if not user_id or not issue:
-        return jsonify({'message': 'Invalid request body', 'success': False}), 400
-
-    # assign ticket to user based on round-robin principle
+    data = request.get_json()
+    user_id = data.get('user_id')
+    issue = data.get('issue')
+    
+    if user_id is None or issue is None:
+        return render_template('error.html', message=message[0], success=False), 400
     if len(users) == 0:
-        return jsonify({'message': 'No users available to assign ticket', 'success': False}), 404
-    assigned_to = users[ticket_counter % len(users)]
-    ticket_counter += 1
+        return render_template('error.html', message = message[2], success = False), 404
+    
+    ticket_id = len(tickets) + 1
+    assigned_to = users[len(tickets) % len(users)]
+    
+    ticket = {
+        'ticket_id': ticket_id,
+        'user_id': user_id,
+        'issue': issue,
+        'assigned_to': assigned_to
+    }
+    
+    tickets.append(ticket)
 
-    # create and return ticket response
-    ticket_id = f'ticket-{ticket_counter}'
-    response_data = {'ticket_id': ticket_id, 'assigned_to': assigned_to}
-    return jsonify({'message': 'Ticket created', 'success': True, 'data': response_data}), 201
+    return render_template('index.html', message=message[1], success=True, data={
+        'ticket_id': ticket_id,
+        'assigned_to': assigned_to
+    }), 201
 
 @app.errorhandler(500)
 def server_error(error):
-    return jsonify({'message': 'Internal server error', 'success': False}), 500
+    return render_template('error.html', message = 'Internal server error', success = False), 500
 
 if __name__ == '__main__':
-    app.run()
+    server_port = os.environ.get('PORT', '8080')
+    app.run(debug=False, port=server_port, host='0.0.0.0')
